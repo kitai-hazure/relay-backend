a7fb9c448c740340edee622aa445cc4b83e63262
kalashshah <202051096@iiitvadodara.ac.in>
2023-02-04 17:02:36 +0530

    :construction: radar api

---
+++ src/radar/radar.resolver.ts
@@ -0,0 +1,17 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Context, Query, Resolver } from '@nestjs/graphql';
+import { Person } from 'src/graphql.types';
+import { verifyJWT } from 'src/utils/jwt.utils';
+import { RadarService } from './radar.service';
+
+@Resolver()
+export class RadarResolver {
+  constructor(private readonly radarService: RadarService) {}
+
+  @Query('findPeople')
+  async findPeople(@Context() context): Promise<Person[]> {
+    const { expired, payload } = verifyJWT(context.req.headers.authorization);
+    if (expired) throw new UnauthorizedException('Please login to continue.');
+    return await this.radarService.findPeople(payload.id);
+  }
+}
